1a495b545918c87b2b5a3291b6788bec8614d74e
MaxFuentes07101 <51061877+MaxFuentes07101@users.noreply.github.com>
2023-05-12 08:55:39 -0700

    Update microphone.py

--- microphone.py
+++ microphone.py
@@ -1,1 +1,22 @@
 
+import speech_recognition as sr
+
+class Microphone:
+    def __init__(self):
+        self.recognizer = sr.Recognizer()
+
+    def listen(self):
+        with sr.Microphone() as source:
+            print("Listening...")
+            audio_data = self.recognizer.listen(source)
+        return audio_data
+
+    def recognize_speech(self, audio_data):
+        try:
+            text = self.recognizer.recognize_google(audio_data)
+            return text
+        except sr.UnknownValueError:
+            return None
+        except sr.RequestError as e:
+            print(f"Error with the speech recognition service: {e}")
+            return None
