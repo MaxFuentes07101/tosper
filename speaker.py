15bb5348b60ee293a113382c949255f68422cf69
MaxFuentes07101 <51061877+MaxFuentes07101@users.noreply.github.com>
2023-05-12 08:53:32 -0700

    Update speaker.py

--- speaker.py
+++ speaker.py
@@ -1,1 +1,17 @@
 
+from gtts import gTTS
+import os
+import pygame
+
+class Speaker:
+    def __init__(self):
+        pygame.mixer.init()
+
+    def speak(self, text):
+        tts = gTTS(text=text, lang='en', slow=False)
+        tts.save("temp.mp3")
+        pygame.mixer.music.load("temp.mp3")
+        pygame.mixer.music.play()
+        while pygame.mixer.music.get_busy():
+            continue
+        os.remove("temp.mp3")
